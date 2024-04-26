1b1bc678f7fb2fac7c074158c2164fa8619438b1
Lucas Moraes <lucasmoraes86@hotmail.com>
2024-04-26 19:53:45 -0300

    Adicionar arquivos de projeto.

---
+++ Integracao/BaseClient.cs
@@ -0,0 +1,19 @@
+﻿namespace Integracao
+{
+    public class BaseClient
+    {
+        static readonly HttpClient _client = new HttpClient();
+        string _baseUrl = "https://rvh98qbf-44321.brs.devtunnels.ms/";
+        
+        public async Task<HttpResponseMessage> GetShare(string shareSymbol)
+        {
+            string end = _baseUrl  + "Share/" + shareSymbol;
+            var response = await _client.GetAsync(end);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Erro ao buscar informações da ação");
+            }
+            return response;
+        }
+    }
+}
---
+++ Prova/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Prova
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void CliqueBuscarInformacoes(object sender, EventArgs e)
+        {
+            string shareSymbol = campoSimbolo.Text;
+            ShareDetails shareDetails = new ShareDetails(shareSymbol);
+            await Navigation.PushAsync(shareDetails);
+            SemanticScreenReader.Announce(BuscarInformacoes.Text);
+        }
+    }
+}
